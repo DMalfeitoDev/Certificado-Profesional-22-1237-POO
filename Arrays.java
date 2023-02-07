fb0d40edff51f8cf9f80bbb9ec5ea878d2db0691
David Pantoja Malfeito <116077831+DMalfeitoDev@users.noreply.github.com>
2023-02-07 16:01:41 +0100

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,17 @@
+
+public class Arrays {
+	public static void main(String[] args) {
+		
+		int [] nums = new int[10];
+		nums[0] = 345;
+		nums[1] = 123;
+		nums[2] = 55;
+		nums[3] = 78;
+		
+		for (int i=0; i <nums.length; i++) {
+		
+			System.out.println(i + ":" + nums[i]);
+		
+		}
+	}
+}
